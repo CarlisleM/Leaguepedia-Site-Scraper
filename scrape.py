5a74a70a95e78e3c9f5b30d867fa44b3ef829fbd
Carlisle Miller <carlisle.m@hotmail.com>
2019-06-15 15:11:15 +1000

    Make function for getting data

--- scrape.py
+++ scrape.py
@@ -6,6 +6,28 @@ import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+
+def process_data(split_objective_data, blue_team, red_team):
+    splitDragonData = []
+
+    for entries in split_objective_data:
+        splitDragonData.append(entries.split())
+
+    counter = 0
+    dragonTimer = []
+
+    for rows in splitDragonData:
+        dragonTimer.append([re.sub("[^0-9.]", "", splitDragonData[counter][4]), re.sub("[^0-9.]", "", splitDragonData[counter][6])])
+        counter = counter+1
+
+    for i in dragonTimer:
+        if i[0] == min(x[0] for x in dragonTimer):
+            firstDragon = int(i[1])
+            if firstDragon == 0:
+                return blue_team
+            else:
+                return red_team
+
 # This section locates all of the match history links
 #url = 'https://lol.gamepedia.com/LCS/2019_Season/Summer_Season'
 #url = 'https://lol.gamepedia.com/LEC/2019_Season/Spring_Season'
@@ -20,7 +42,6 @@ print('Starting the scraper')
 matchHistoryLinks = []
 
 for link in soup.find_all('a', attrs={'href': re.compile("matchhistory")}):
-    print("Rnning app")
     matchHistoryLinks.append(link.get('href'))
 
 #This section retrieves data from each match history link
@@ -28,175 +49,94 @@ outfile = open("./LeagueData.csv", "w")
 writer = csv.writer(outfile)
 writer.writerow(['Date', 'Team 1', 'Team 2', 'First Blood', 'First Dragon', 'First Turret', 'First Inhibitor', 'First Baron', 'Result of Team 1'])
 
-for link in matchHistoryLinks:
-	options = webdriver.ChromeOptions()
-	options.add_argument('--ignore-certificate-errors')
-	options.add_argument('--incognito')
-	options.add_argument('--headless')
-	driverLocation = str(sys.argv[1])
-	print(driverLocation)
-	driver = webdriver.Chrome(executable_path=driverLocation, chrome_options=options)
+for link in [matchHistoryLinks[0]]:
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    driverLocation = str(sys.argv[1])
+    print(driverLocation)
+    driver = webdriver.Chrome(executable_path=driverLocation, options=options)
 
 
-	driver.get(link)
-	time.sleep(3) # Allows the dynamic data on the page to load
-	page_source = driver.page_source
+    driver.get(link)
+    time.sleep(3) # Allows the dynamic data on the page to load
+    page_source = driver.page_source
 
-	soup = BeautifulSoup(page_source, 'html.parser')
+    soup = BeautifulSoup(page_source, 'html.parser')
 
-	print('Currently scraping: ', link)
+    gameDate = soup.find("div", {"id": "binding-699"}).text # Date the match was played
+    team1 = (soup.find('div', attrs={'id':'binding-739'}).text).split() # Team 1 name
+    team2 = (soup.find('div', attrs={'id':'binding-881'}).text).split() # Team 2 name
+    gameResults = soup.find('div', attrs={'class':'game-conclusion'}).text # Winner/Loser
 
-	gameDate = soup.find("div", {"id": "binding-699"}).text # Date the match was played
-	team1 = (soup.find('div', attrs={'id':'binding-739'}).text).split() # Team 1 name
-	team2 = (soup.find('div', attrs={'id':'binding-881'}).text).split() # Team 2 name
-	gameResults = soup.find('div', attrs={'class':'game-conclusion'}).text # Winner/Loser
+    if str(gameResults.strip()) in 'VICTORY':
+        gameResults = team1
+    else:
+        gameResults = team2
 
-	if str(gameResults.strip()) in 'VICTORY':
-		gameResults = team1
-	else:
-		gameResults = team2
+    # Obtain dragon, turret, and first blood info
+    objectiveData = []
 
-	# Obtain dragon, turret, and first blood info
-	objectiveData = []
+    for lines in soup.findAll('image'):
+        objectiveData.append(str(lines))
 
-	for lines in soup.findAll('image'):
-		objectiveData.append(str(lines))
+    riftheraldData = [a for a in objectiveData if "riftherald" in a]
+    dragonData = [b for b in objectiveData if "dragon" in b]
+    baronData = [c for c in objectiveData if "baron" in c]
+    turretData = turretData = [e for e in objectiveData if "turret" in e]
+    inhibitorData = [d for d in objectiveData if "inhibitor" in d]
 
-	riftheraldData = [a for a in objectiveData if "riftherald" in a]
-	dragonData = [b for b in objectiveData if "dragon" in b]
-	baronData = [c for c in objectiveData if "baron" in c]
-	turretData = turretData = [e for e in objectiveData if "turret" in e]
-	inhibitorData = [d for d in objectiveData if "inhibitor" in d]
+    # print(*riftheraldData, sep = "\n")
+    # print(*dragonData, sep = "\n")
+    # print(*baronData, sep = "\n")
+    # print(*inhibitorData, sep = "\n")
+    # print(*turretData, sep = "\n")
 
-	print(*riftheraldData, sep = "\n")
-	print(*dragonData, sep = "\n")
-	print(*baronData, sep = "\n")
-	print(*inhibitorData, sep = "\n")
-	print(*turretData, sep = "\n")
+    # First Blood
+    collectStatistics = []
 
-	# First Blood
-	collectStatistics = []
+    rows = soup.find_all('tr')
+    for row in rows:          # Print all occurrences
+        collectStatistics.append(row.get_text())
 
-	rows = soup.find_all('tr')
-	for row in rows:          # Print all occurrences
-		collectStatistics.append(row.get_text())
+    determineFB = re.sub(r'[a-zA-Z]+', '', collectStatistics[5], re.I)
+    firstBlood = determineFB.split('●')[0]
 
-	determineFB = re.sub(r'[a-zA-Z]+', '', collectStatistics[5], re.I)
-	firstBlood = determineFB.split('●')[0]
+    if int(firstBlood.count('○')) < 5:
+        firstBlood = team1[0].strip()
+    else:
+        firstBlood = team2[0].strip()
 
-	if int(firstBlood.count('○')) < 5:
-		firstBlood = team1[0].strip()
-	else:
-		firstBlood = team2[0].strip()
 
-	# Dragon
-	splitDragonData = []
+    firstDragon = process_data(dragonData, team1, team2)
+    firstTurret = process_data(turretData, team1, team2)
+    firstBaron = process_data(baronData, team1, team2)
+    firstInhibitor = process_data(inhibitorData, team1, team2)
 
-	for entries in dragonData:
-		splitDragonData.append(entries.split())
+    # Append to file
+    gameData = []
 
-	counter = 0
-	dragonTimer = []
+    # print('Debug')
+    # print(gameDate.strip())
+    # print(team1[0].strip())
+    # print(team2[0].strip())
+    # print('First Blood')
+    # print(firstDragon[0].strip())
+    # print(firstTurret[0].strip())
+    # print(firstInhibitor[0].strip())
+    # print(firstBaron[0].strip())
+    # print(gameResults[0].strip())
 
-	for rows in splitDragonData:
-		dragonTimer.append([re.sub("[^0-9.]", "", splitDragonData[counter][4]), re.sub("[^0-9.]", "", splitDragonData[counter][6])])
-		counter = counter+1
+    try:
+        gameData.append([gameDate.strip(), team1[0].strip(), team2[0].strip(), firstBlood, firstDragon[0].strip(), firstTurret[0].strip(), firstInhibitor[0].strip(), firstBaron[0].strip(), gameResults[0].strip()])
+    except IndexError:
+        gameData.append(['Index out of bound error'])
+        print('index out of bounds error')
 
-	for i in dragonTimer:
-		if i[0] == min(x[0] for x in dragonTimer):
-			firstDragon = int(i[1])
-			if firstDragon == 0:
-				firstDragon = team1
-			else:
-				firstDragon = team2
+    writer.writerows(gameData)
 
-	# Baron
-	splitBaronData = []
-
-	for entries in baronData:
-		splitBaronData.append(entries.split())
-
-	counter = 0
-	baronTimer = []
-
-	for rows in splitBaronData:
-		baronTimer.append([re.sub("[^0-9.]", "", splitBaronData[counter][4]), re.sub("[^0-9.]", "", splitBaronData[counter][6])])
-		counter = counter+1
-
-	for i in baronTimer:
-		if i[0] == min(x[0] for x in baronTimer):
-			firstBaron = int(i[1])
-			if firstBaron == 0:
-				firstBaron = team1
-			else:
-				firstBaron = team2
-
-	# Turret
-	splitTurretData = []
-
-	for entries in turretData:
-		splitTurretData.append(entries.split())
-
-	counter = 0
-	turretTimer = []
-
-	for rows in splitTurretData:
-		turretTimer.append([re.sub("[^0-9.]", "", splitTurretData[counter][4]), re.sub("[^0-9.]", "", splitTurretData[counter][6])])
-		counter = counter+1
-
-	for i in turretTimer:
-		if i[0] == min(x[0] for x in turretTimer):
-			firstTurret = int(i[1])
-			if firstTurret == 0:
-				firstTurret = team1
-			else:
-				firstTurret = team2
-
-	# Inhibitor
-	splitInhibitorData = []
-
-	for entries in inhibitorData:
-		splitInhibitorData.append(entries.split())
-
-	counter = 0
-	inhibitorTimer = []
-
-	for rows in splitInhibitorData:
-		inhibitorTimer.append([re.sub("[^0-9.]", "", splitInhibitorData[counter][4]), re.sub("[^0-9.]", "", splitInhibitorData[counter][6])])
-		counter = counter+1
-
-	for i in inhibitorTimer:
-		if i[0] == min(x[0] for x in inhibitorTimer):
-			firstInhibitor = int(i[1])
-			if firstInhibitor == 0:
-				firstInhibitor = team1
-			else:
-				firstInhibitor = team2
-
-	# Append to file
-	gameData = []
-
-	# print('Debug')
-	# print(gameDate.strip())
-	# print(team1[0].strip())
-	# print(team2[0].strip())
-	# print('First Blood')
-	# print(firstDragon[0].strip())
-	# print(firstTurret[0].strip())
-	# print(firstInhibitor[0].strip())
-	# print(firstBaron[0].strip())
-	# print(gameResults[0].strip())
-
-	try:
-		gameData.append([gameDate.strip(), team1[0].strip(), team2[0].strip(), firstBlood, firstDragon[0].strip(), firstTurret[0].strip(), firstInhibitor[0].strip(), firstBaron[0].strip(), gameResults[0].strip()])
-		print(gameData)
-	except IndexError:
-		gameData.append(['Index out of bound error'])
-		print('index out of bounds error')
-
-	writer.writerows(gameData)
-
-	print('Done')
+    print('Done')
 
 print('Finished')
 
