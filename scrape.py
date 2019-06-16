aad9fc6e07b86a911d516d8f09b020c47899f6a8
Carlisle Miller <carlisle.m@hotmail.com>
2019-06-16 12:03:23 +1000

    Now skips over games that do not have match history, includes game count for bo3/bo5 sets

--- scrape.py
+++ scrape.py
@@ -5,8 +5,15 @@ import time
 import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import timeit
 
-def get_page_source(link, time_to_sleep):
+start = timeit.default_timer()
+
+def get_page_source(link):
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
@@ -15,8 +22,16 @@ def get_page_source(link, time_to_sleep):
     driver = webdriver.Chrome(executable_path=driverLocation, options=options)
 
     driver.get(link)
-    time.sleep(time_to_sleep) # Allows the dynamic data on the page to load
-    return driver.page_source
+
+    wait = 5 # seconds
+    try:
+        wait_for_graph = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.ID, 'event-graph-987')))
+        page_loaded = 'ready'
+    except TimeoutException:
+        page_loaded = 'not ready'
+        print ("Loading took too long!")
+
+    return driver.page_source, page_loaded
 
 def process_data(split_objective_data, blue_team, red_team):
     split_data = []
@@ -41,9 +56,14 @@ def process_data(split_objective_data, blue_team, red_team):
 
 # This section locates all of the match history links
 #url = 'https://lol.gamepedia.com/LCS/2019_Season/Summer_Season'
-url = 'https://lol.gamepedia.com/LEC/2019_Season/Spring_Season'
-#url = 'https://lol.gamepedia.com/LMS/2019_Season/Summer_Season'
+#url = 'https://lol.gamepedia.com/LEC/2019_Season/Spring_Season'
+#url = 'https://lol.gamepedia.com/OPL/2019_Season/Split_1'
+#url = 'https://lol.gamepedia.com/OPL/2019_Season/Split_2'
+#url = 'https://lol.gamepedia.com/TCL/2019_Season/Winter_Season'
+#url = 'https://lol.gamepedia.com/LVP_SuperLiga_Orange/2019_Season/Spring_Season'
+url = 'https://lol.gamepedia.com/LMS/2019_Season/Summer_Season'
 #url = 'https://lol.gamepedia.com/LEC/2019_Season/Summer_Season'
+#url = 'https://lol.gamepedia.com/LVP_SuperLiga_Orange/2019_Season/Summer_Season'
 response = requests.get(url)
 html = response.content
 
@@ -61,100 +81,125 @@ outfile = open("./LeagueData.csv", "w")
 writer = csv.writer(outfile)
 writer.writerow(['Date', 'Team 1', 'Team 2', 'First Blood', 'First Dragon', 'First Turret', 'First Inhibitor', 'First Baron', 'Winner', 'Loser'])
 
+count = 1
+previous_game_data = ['1','2','3']
+
 for link in matchHistoryLinks:
-    page_source = get_page_source(link, 3)
 
-    soup = BeautifulSoup(page_source, 'html.parser')
+    page_info = get_page_source(link)
+    page_source = page_info[0]
+    page_status = page_info[1]
 
-   # print('Testing here')
-   # print(soup.find('div', attrs={"id": "champion-nameplate-138"}).text)
+    if page_status == 'ready':
 
-    gameDate = soup.find("div", {"id": "binding-699"}).text # Date the match was played
-    #team1 = (soup.find('div', attrs={'id':'binding-739'}).text).split() # Team 1 name
-    #team2 = (soup.find('div', attrs={'id':'binding-881'}).text).split() # Team 2 name
-    team1 = (soup.find('div', attrs={"id": "champion-nameplate-16"}).text).split() # Team 1 name
-    team2 = (soup.find('div', attrs={"id": "champion-nameplate-138"}).text).split() # Team 2 name
-    gameWinner = soup.find('div', attrs={'class':'game-conclusion'}).text # Winner/Loser
+        soup = BeautifulSoup(page_source, 'html.parser')
 
-    if str(gameWinner.strip()) in 'VICTORY':
-        gameWinner = team1
-        gameLoser = team2
+        gameDate = soup.find("div", {"id": "binding-699"}).text # Date the match was played
+        team1 = (soup.find('div', attrs={"id": "champion-nameplate-16"}).text).split() # Team 1 name
+        team2 = (soup.find('div', attrs={"id": "champion-nameplate-138"}).text).split() # Team 2 name
+        gameWinner = soup.find('div', attrs={'class':'game-conclusion'}).text # Winner/Loser
+
+        if gameDate == previous_game_data[0] and team1[0].strip() == previous_game_data[1] or team1[0].strip() == previous_game_data[2] and team2[0].strip() == previous_game_data[1] or team2[0].strip() == previous_game_data[2]:
+            if gameCount == 1:
+                gameCount = 2
+            elif gameCount == 2:
+                gameCount = 3
+            elif gameCount == 3:
+                gameCount = 4
+            elif gameCount == 4:
+                gameCount = 5
+        else:
+            gameCount = 1
+
+        print(gameDate + ' ' + team1[0].strip() + ' ' + team2[0].strip() + ' ' + 'Game: ' + str(gameCount))
+
+        if str(gameWinner.strip()) in 'VICTORY':
+            gameWinner = team1
+            gameLoser = team2
+        else:
+            gameWinner = team2
+            gameLoser = team1
+
+        # Obtain dragon, turret, and first blood info
+        objectiveData = []
+
+        for lines in soup.findAll('image'):
+            objectiveData.append(str(lines))
+
+        riftheraldData = [a for a in objectiveData if "riftherald" in a]
+        dragonData = [b for b in objectiveData if "dragon" in b]
+        baronData = [c for c in objectiveData if "baron" in c]
+        turretData = turretData = [e for e in objectiveData if "turret" in e]
+        inhibitorData = [d for d in objectiveData if "inhibitor" in d]
+
+        # First Blood
+        collectStatistics = []
+
+        rows = soup.find_all('tr')
+        for row in rows:          # Print all occurrences
+            collectStatistics.append(row.get_text())
+
+        determineFB = re.sub(r'[a-zA-Z]+', '', collectStatistics[5], re.I)
+        firstBlood = determineFB.split('●')[0]
+
+        if int(firstBlood.count('○')) < 5:
+            firstBlood = team1[0].strip()
+        else:
+            firstBlood = team2[0].strip()
+
+        if not dragonData:
+            firstDragon = ' '
+        else: 
+            firstDragon = process_data(dragonData, team1, team2)
+
+        if not turretData:
+            firstTurret = ' '
+        else:
+            firstTurret = process_data(turretData, team1, team2)
+        
+        if not baronData:
+            firstBaron = ' '
+        else:
+            firstBaron = process_data(baronData, team1, team2)
+
+        if not inhibitorData:
+            firstInhibitor = ' '
+        else:
+            firstInhibitor = process_data(inhibitorData, team1, team2)
+
+        # Append to file
+        gameData = []
+
+        # print('Debug')
+        # print(gameDate)
+    #     print(gameDate.strip())
+    #     print(team1[0].strip())
+    #     print(team2[0].strip())
+    #     print('First Blood')
+    #     print(firstDragon[0].strip())
+    #     print(firstTurret[0].strip())
+    # #    print(inhibitorData)
+    #     print(firstInhibitor[0].strip())
+    # #    print(baronData)
+    #     print(firstBaron[0].strip())
+    #     print(gameWinner[0].strip())
+
+        try:
+            gameData.append([gameDate.strip(), team1[0].strip(), team2[0].strip(), firstBlood, firstDragon[0].strip(), firstTurret[0].strip(), firstInhibitor[0].strip(), firstBaron[0].strip(), gameWinner[0].strip(), gameLoser[0].strip()])
+            previous_game_data = [gameDate.strip(), team1[0].strip(), team2[0].strip()]
+        except IndexError:
+            gameData.append(['Index out of bound error'])
+            print('index out of bounds error')
+
+        writer.writerows(gameData)
+
+        print('Done: ' + str(count))
+        count = count+1
     else:
-        gameWinner = team2
-        gameLoser = team1
-
-    # Obtain dragon, turret, and first blood info
-    objectiveData = []
-
-    for lines in soup.findAll('image'):
-        objectiveData.append(str(lines))
-
-    riftheraldData = [a for a in objectiveData if "riftherald" in a]
-    dragonData = [b for b in objectiveData if "dragon" in b]
-    baronData = [c for c in objectiveData if "baron" in c]
-    turretData = turretData = [e for e in objectiveData if "turret" in e]
-    inhibitorData = [d for d in objectiveData if "inhibitor" in d]
-
-    # First Blood
-    collectStatistics = []
-
-    rows = soup.find_all('tr')
-    for row in rows:          # Print all occurrences
-        collectStatistics.append(row.get_text())
-
-    determineFB = re.sub(r'[a-zA-Z]+', '', collectStatistics[5], re.I)
-    firstBlood = determineFB.split('●')[0]
-
-    if int(firstBlood.count('○')) < 5:
-        firstBlood = team1[0].strip()
-    else:
-        firstBlood = team2[0].strip()
-
-    if not dragonData:
-        firstDragon = ' '
-    else: 
-        firstDragon = process_data(dragonData, team1, team2)
-
-    if not turretData:
-        firstTurret = ' '
-    else:
-        firstTurret = process_data(turretData, team1, team2)
-    
-    if not baronData:
-        firstBaron = ' '
-    else:
-        firstBaron = process_data(baronData, team1, team2)
-
-    if not inhibitorData:
-        firstInhibitor = ' '
-    else:
-        firstInhibitor = process_data(inhibitorData, team1, team2)
-
-    # Append to file
-    gameData = []
-
-    # print('Debug')
-    # print(gameDate.strip())
-    # print(team1[0].strip())
-    # print(team2[0].strip())
-    # print('First Blood')
-    # print(firstDragon[0].strip())
-    # print(firstTurret[0].strip())
-    # print(inhibitorData)
-    # print(firstInhibitor[0].strip())
-    # print(baronData)
-    # print(firstBaron[0].strip())
-    # print(gameWinner[0].strip())
-
-    try:
-        gameData.append([gameDate.strip(), team1[0].strip(), team2[0].strip(), firstBlood, firstDragon[0].strip(), firstTurret[0].strip(), firstInhibitor[0].strip(), firstBaron[0].strip(), gameWinner[0].strip(), gameLoser[0].strip()])
-    except IndexError:
-        gameData.append(['Index out of bound error'])
-        print('index out of bounds error')
-
-    writer.writerows(gameData)
-
-    print('Done')
+        print('Skipped')
 
 print('Finished')
+stop = timeit.default_timer()
+
+print('Time: ', stop - start) 
 
